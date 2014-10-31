669c58d34569a18a9b23646b3babc872262054d0
fabian-garzon <monse40@gmail.com>
2014-10-31 13:49:15 -0500

    Add account activation, password resets & email configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
+  get 'account_activations/edit'
+
   get 'sessions/new'
 
   get 'users/new'
@@ -22,6 +28,9 @@ Rails.application.routes.draw do
 
   resources :users
 
+  resources :account_activations, only: [:edit]
+  
+  resources :password_resets, only: [:new, :create, :edit, :update]
 
 
 
---
+++ db/migrate/20141031180255_add_reset_at_to_users.rb
@@ -0,0 +1,5 @@
+class AddResetAtToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :reset_sent_at, :datetime
+  end
+end
